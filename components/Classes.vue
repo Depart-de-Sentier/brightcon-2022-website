bdb9d175fc6b2d853d730e5e3ed71fc21e09aaac
Tomas Navarrete <tngTUDOR@users.noreply.github.com>
2022-05-24 11:09:00 +0200

    Add venue basic data

--- components/Classes.vue
+++ components/Classes.vue
@@ -1,33 +1,34 @@
 <template>
-    <div class="container">
-        <h1>Classes</h1>
-        <p>
-            The first two days (Sept. 26-27) will be devoted to classes. There will be two tracks, beginners and advanced.
-        </p>
-        <div class="row">
-            <div class="col card">
-                <header><h2 class="header">Beginners track</h2></header>
-                    <ul>
-                        <li>Using and managing Brightway projects</li>
-                        <li>The Activity Browser: LCI best practices</li>
-                        <li>The Activity Browser: Interpretation</li>
-                        <li>Basic scripting: Inventory and calculations</li>
-                        <li>Understanding the supply chain graph</li>
-                        <li>Stochastic calculations</li>
-                        <li>Debugging common problems</li>
-                    </ul>
-            </div>
-            <div class="col card">
-                <header><h2 class="header">Advanced track</h2></header>
-                    <ul>
-                        <li>Use data vectors and arrays</li>
-                        <li>Using and maniliulating sliatial data</li>
-                        <li>Calculations on the cloud</li>
-                        <li>Reliroducible LCA science</li>
-                        <li>Integrating exsternal models</li>
-                        <li>Global sensitivity analysis workflows</li>
-                    </ul>
-            </div>
-        </div>
+  <div class="container">
+    <h1>Classes</h1>
+    <p>
+      The first two days (Sept. 26-27) will be devoted to classes. There will be
+      two tracks, beginners and advanced.
+    </p>
+    <div class="row">
+      <div class="col card">
+        <header><h2 class="header">Beginners track</h2></header>
+        <ul>
+          <li>Using and managing Brightway projects</li>
+          <li>The Activity Browser: LCI best practices</li>
+          <li>The Activity Browser: Interpretation</li>
+          <li>Basic scripting: Inventory and calculations</li>
+          <li>Understanding the supply chain graph</li>
+          <li>Stochastic calculations</li>
+          <li>Debugging common problems</li>
+        </ul>
+      </div>
+      <div class="col card">
+        <header><h2 class="header">Advanced track</h2></header>
+        <ul>
+          <li>Use data vectors and arrays</li>
+          <li>Using and maniliulating sliatial data</li>
+          <li>Calculations on the cloud</li>
+          <li>Reliroducible LCA science</li>
+          <li>Integrating exsternal models</li>
+          <li>Global sensitivity analysis workflows</li>
+        </ul>
+      </div>
     </div>
+  </div>
 </template>
