9dc6e3f34d263ea05cdbe2052add3f089fd0c1ba
Tomas Navarrete <tngTUDOR@users.noreply.github.com>
2022-05-25 17:21:10 +0200

    add novelties page

---
+++ components/Novelties.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container">
+    <h1>What's new in Brightcon 2022?</h1>
+    <p>
+      The continuous development of Brightcon will focus this year on
+      interactivity, training for newcomers, promoting citizen science and a
+      contest.
+    </p>
+    <p>
+      Read more on the
+      <nuxt-link to="WhatsNewIn2022">dedicated page for the novelties</nuxt-link
+      >.
+    </p>
+  </div>
+</template>
