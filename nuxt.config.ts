d8c1c064416cd4627e69964473cbe979575c89be
Laurent Chion <laurent.chion@list.lu>
2022-05-25 13:34:28 +0200

    Run prettier

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,12 +3,14 @@ import { defineNuxtConfig } from "nuxt";
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 
 export default defineNuxtConfig({
-    meta: {
-        title: "Brightcon 2022",
-        link: [
-            {
-                rel: "icon", type:'image/x-icon', href:'/images/favicon-32x32.png'
-            }
-        ]
-    }
-})
+  meta: {
+    title: "Brightcon 2022",
+    link: [
+      {
+        rel: "icon",
+        type: "image/x-icon",
+        href: "/images/favicon-32x32.png",
+      },
+    ],
+  },
+});
