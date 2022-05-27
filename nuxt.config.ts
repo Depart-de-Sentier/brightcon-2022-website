48e1603a5ac37501c0790845546c8cb16d75bc4e
Laurent Chion <laurent.chion@list.lu>
2022-05-27 16:13:53 +0200

    Add @nuxt/sitemap plugin

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,12 +5,38 @@ import { defineNuxtConfig } from "nuxt";
 export default defineNuxtConfig({
   meta: {
     title: "Brightcon 2022",
+    meta: [
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'Brightcon is an annual conference where the community of the open source framework for LCA Brightway meet. The conference enables interactions between Brightway developers, maintainers, contributors, users and adopters.'
+      }
+    ],
     link: [
       {
         rel: "icon",
         type: "image/x-icon",
-        href: "/images/favicon-32x32.png",
+        href: "/favicon.ico",
       },
     ],
   },
+  modules:[
+    '@nuxtjs/sitemap'
+  ],
+  sitemap: {
+    hostname: 'https://2022.brightcon.link',
+    gzip: true,
+    routes: [
+      '/',
+      '/calls',
+      '/legal',
+      '/programme',
+      '/register',
+      '/sponsors',
+      '/venue',
+      '/WhatsNewIn2022',
+    ]
+  },
+
 });
