f642c591ccb2e6bdc699600cfe3432483e1350e7
Tomas Navarrete <tngTUDOR@users.noreply.github.com>
2022-08-25 17:47:03 +0200

    📝 Update information for classes.
    Add information about what to bring for classes.

--- components/Classes.vue
+++ components/Classes.vue
@@ -5,11 +5,36 @@
       The first two days (Sept. 26-27) will be devoted to classes. There will be
       two tracks, beginners and advanced.
     </p>
+    <div class="indent bg-light">
+      <h2>You will need</h2>
+      <p>
+        We want to make the classes as productive and interactive as possible.
+        <br />
+        For this, we will be using a jupyter hub instance managed by
+        Depart-de-Sentier where brightway and jupyter are already up and
+        running. This means that <strong>you will be able to start using Brightway
+        immediately during the classes</strong>.
+        <br />
+        <strong>All you need is your own computer and a web-browser</strong>.
+        For the Activity Browser related parts of the classes, we will be there
+        ready to help you install it in your laptop, but if you can come with a
+        working installation of it, it will save us all some time. Take a look
+        at the documentation of the
+        <a
+          href="https://github.com/LCA-ActivityBrowser/activity-browser#installation"
+        >
+          Activity Browser</a
+        >
+        for installation instructions.
+      </p>
+    </div>
+    <br />
     <div class="row">
       <div class="col card">
         <header>
           <h2 class="header">Beginners track</h2>
         </header>
+        <p>Requirements. python basics, numpy basics, basics of LCA.</p>
         <ul>
           <li>Using and managing Brightway projects</li>
           <li>The Activity Browser: LCI best practices</li>
@@ -36,3 +61,8 @@
     </div>
   </div>
 </template>
+<style>
+.indent {
+  padding: 20px;
+}
+</style>
