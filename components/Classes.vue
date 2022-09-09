4a71f4f6551e0aa35cc05b1479421cf12de9481b
Tomas Navarrete <tngTUDOR@users.noreply.github.com>
2022-09-09 18:40:08 +0200

    💬 Update provisional classes schedule
    Each track of the classes has more details about the timing and topics.

--- components/Classes.vue
+++ components/Classes.vue
@@ -10,16 +10,22 @@
       <p>
         We want to make the classes as productive and interactive as possible.
         <br />
-        For this, we will be using a jupyter hub instance managed by
+        For this, we will be using a jupyter lab hub instance managed by
         Depart-de-Sentier where brightway and jupyter are already up and
-        running. This means that <strong>you will be able to start using Brightway
-        immediately during the classes</strong>.
+        running. This means that
+        <strong
+          >you will be able to start using Brightway immediately during the
+          classes</strong
+        >. Look for the
+        <span class="tag is-small bd-dark">jupyter lab</span> next to the topic
+        where the jupyter lab will be used.
         <br />
         <strong>All you need is your own computer and a web-browser</strong>.
-        For the Activity Browser related parts of the classes, we will be there
-        ready to help you install it in your laptop, but if you can come with a
-        working installation of it, it will save us all some time. Take a look
-        at the documentation of the
+        For the Activity Browser related parts of the classes, (look for the
+        <span class="tag is-small bd-dark">AB</span> tag next to the topic) we
+        will be there ready to help you install it in your laptop, but if you
+        can come with a working installation of it, it will save us all some
+        time. Take a look at the documentation of the
         <a
           href="https://github.com/LCA-ActivityBrowser/activity-browser#installation"
         >
@@ -29,38 +35,263 @@
       </p>
     </div>
     <br />
-    <div class="row">
-      <div class="col card">
-        <header>
-          <h2 class="header">Beginners track</h2>
-        </header>
-        <p>Requirements. python basics, numpy basics, basics of LCA.</p>
-        <ul>
-          <li>Using and managing Brightway projects</li>
-          <li>The Activity Browser: LCI best practices</li>
-          <li>The Activity Browser: Interpretation</li>
-          <li>Basic scripting: Inventory and calculations</li>
-          <li>Understanding the supply chain graph</li>
-          <li>Stochastic calculations</li>
-          <li>Debugging common problems</li>
-        </ul>
+    <div v-for="(day, i) in sessionsMonday" :key="i">
+      <header>
+        <h3>Provisional schedule: {{ day.date }}</h3>
+      </header>
+      <div class="row">
+        <div class="col card">
+          <header><h3>Beginners track</h3></header>
+          <table>
+            <thead>
+              <tr>
+                <th>Time</th>
+                <th>Topic</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr
+                v-for="(session, session_id) in day.beginnerSessions"
+                :key="session_id"
+              >
+                <td>{{ session.time }}</td>
+                <td>
+                  {{ session.topic }}
+                  <div v-if="session.platform">
+                    <span class="tag is-small">{{ session.platform }}</span>
+                  </div>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <div class="col card">
+          <header><h3>Advanced track</h3></header>
+          <table>
+            <thead>
+              <tr>
+                <th>Time</th>
+                <th>Topic</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr
+                v-for="(session, session_id) in day.advancedSessions"
+                :key="session_id"
+              >
+                <td>{{ session.time }}</td>
+                <td>
+                  {{ session.topic }}
+                  <div v-if="session.platform">
+                    <span class="tag is-small">{{ session.platform }}</span>
+                  </div>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
-      <div class="col card">
-        <header>
-          <h2 class="header">Advanced track</h2>
-        </header>
-        <ul>
-          <li>Use data vectors and arrays</li>
-          <li>Using and manipulating spatial data</li>
-          <li>Calculations on the cloud</li>
-          <li>Reproducible LCA science</li>
-          <li>Integrating external models</li>
-          <li>Global sensitivity analysis workflows</li>
-        </ul>
+    </div>
+    <div v-for="(day, i) in sessionsTuesday" :key="i">
+      <header>
+        <h3>Provisional schedule: {{ day.date }}</h3>
+      </header>
+      <div class="row">
+        <div class="col card">
+          <header><h3>Beginners track</h3></header>
+          <table>
+            <thead>
+              <tr>
+                <th>Time</th>
+                <th>Topic</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr
+                v-for="(session, session_id) in day.beginnerSessions"
+                :key="session_id"
+              >
+                <td>{{ session.time }}</td>
+                <td>
+                  {{ session.topic }}
+                  <div v-if="session.platform">
+                    <span class="tag is-small">{{ session.platform }}</span>
+                  </div>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <div class="col card">
+          <header><h3>Advanced track</h3></header>
+          <table>
+            <thead>
+              <tr>
+                <th>Time</th>
+                <th>Topic</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr
+                v-for="(session, session_id) in day.advancedSessions"
+                :key="session_id"
+              >
+                <td>{{ session.time }}</td>
+                <td>
+                  {{ session.topic }}
+                  <div v-if="session.platform">
+                    <span class="tag is-small">{{ session.platform }}</span>
+                  </div>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   </div>
 </template>
+<script lang="ts">
+import Vue from 'vue'
+
+interface Session {
+  track: string
+  time: string
+  topic: string
+  trainers?: string
+  platform?: string
+}
+
+interface Day {
+  date: string
+  beginnerSessions: Session[]
+  advancedSessions: Session[]
+}
+
+interface Data {
+  sessionsMonday: Day[]
+  sessionsTuesday: Day[]
+}
+
+export default Vue.extend({
+  data(): Data {
+    return {
+      sessionsMonday: [
+        {
+          date: 'Monday, Sept. 26',
+          beginnerSessions: [
+            {
+              track: 'Beginners',
+              time: '9:30-11:15',
+              topic: 'Projects, databases, activities, exchanges.',
+              trainers: 'TBC',
+              platform: 'jupyter lab',
+            },
+            {
+              track: 'Beginners',
+              time: '12:35-13:30',
+              topic: 'Lunch + Activity Browser install fest',
+            },
+            {
+              track: 'Beginners',
+              time: '13:30-15:00',
+              topic: 'Calculations and Interpretations in Activity Browser',
+              platform: 'AB',
+            },
+            {
+              track: 'Beginners',
+              time: '15:15-16:15',
+              topic: 'Effective interaction with the Activity Browser',
+              platform: 'AB',
+            },
+          ],
+          advancedSessions: [
+            {
+              track: 'Advanced',
+              time: '9:30-11:15',
+              topic: 'Brightway 2.5 - What has changed?',
+              platform: 'jupyter lab',
+            },
+            {
+              track: 'Advanced',
+              time: '11:15-12:35',
+              topic: 'Data vectors and arrays, resource groups and interfaces.',
+              platform: 'jupyter lab',
+            },
+            {
+              track: 'Advanced',
+              time: '12:35-13:30',
+              topic: 'Lunch',
+            },
+            {
+              track: 'Advanced',
+              time: '13:30-15:00',
+              topic: 'Integrating external models (TBC)',
+            },
+            {
+              track: 'Advanced',
+              time: '15:15-16:00',
+              topic: 'Calculations in the cloud (TBC)',
+            },
+          ],
+        },
+      ],
+      sessionsTuesday: [
+        {
+          date: 'Tuesday, Sept. 27',
+          beginnerSessions: [
+            {
+              track: 'Beginners',
+              time: '09:30-11:15',
+              topic: 'Data Input Output',
+              platform: 'jupyter lab',
+            },
+            {
+              track: 'Beginners',
+              time: '11:15-12:35',
+              topic: 'Data Input Output',
+              platform: 'jupyter lab',
+            },
+            {
+              track: 'Beginners',
+              time: '13:30-15:05',
+              topic: 'Basic Data Manipulation',
+              platform: 'jupyter lab',
+            },
+          ],
+          advancedSessions: [
+            {
+              track: 'Advanced',
+              time: '9:30-11:00',
+              topic: 'Regionalization part 1',
+              platform: 'jupyter lab',
+            },
+            {
+              track: 'Advanced',
+              time: '11:15-12:35',
+              topic: 'Regionalization part 2',
+              platform: 'jupyter lab',
+            },
+            {
+              track: 'Advanced',
+              time: '13:30-15:00',
+              topic: 'Activity Browser fun with Marc',
+              trainers: 'Marc van der Meide',
+              platform: 'AB',
+            },
+            {
+              track: 'Advanced',
+              time: '15:15-16:00',
+              topic: 'Reproducible science with Brightway (TBC)',
+              platform: 'jupyter lab',
+            },
+          ],
+        },
+      ],
+    }
+  },
+})
+</script>
 <style>
 .indent {
   padding: 20px;
